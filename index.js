aedbea8518ce48940d7e0fb4c6dac806f272c485
DIVYANSH SINGH RATHORE <115221626+eaglex12@users.noreply.github.com>
2024-03-13 18:31:13 +0530

    hiding the keys

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const clientId = 'd1c4eed6-064d-464a-b3d7-d448ad30d036';
-const clientSecret = '15tud2rgx8';
-const redirectUri = 'http://localhost:3000';
+const clientId = '';
+const clientSecret = '';
+const redirectUri = '';
 const responseType='code';
-const authCode = 'Q5o3iy';
-const accessToken='eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI2TUFFR1EiLCJqdGkiOiI2NWYxOGU1ODBiZmI5YzU3NThhOTgyOWUiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNBY3RpdmUiOnRydWUsInNjb3BlIjpbImludGVyYWN0aXZlIiwiaGlzdG9yaWNhbCJdLCJpYXQiOjE3MTAzMjk0MzIsImlzcyI6InVkYXBpLWdhdGV3YXktc2VydmljZSIsImV4cCI6MTcxMDM2NzIwMH0.F6mgvV9oDs9DiLSDAe0mgyiGh-RQ4hPPIrJSmTQ9210'
+const authCode = '';
+const accessToken=''
 const instrumentKeys = 'NSE_EQ:NHPC'; 
 
 
